13694a2f3b2a45424e66c96bc279150c87d4bdd0
Rajan Kumar <rajan.kumar@logicielsolutions.co.in>
2019-09-10 20:01:29 +0530

    APR Calculator

---
+++ apr.js
@@ -0,0 +1,23 @@
+/*!
+ * Javascript APR 
+ **/
+
+function APR(amount, time, interestRrate) {
+	this.amount = amount;
+	this.time = time;
+	this.ir   = interestRrate;
+};
+
+
+APR.prototype.getMonthlyAmount = function(first_argument) {
+	var ir = this.ir
+		time = this.time;
+
+	ir = ir*1/100;
+	ir = ir/12;
+
+	var up = ir*(Math.pow( ir+1, time))
+	var dw = Math.pow( 1+ir, time)-1
+
+	return ( this.amount * ( up / dw ) ).toFixed(2);
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+## APR Calculations
+
+When applying for loans, aside from interest, it is not uncommon for lenders to charge additional fees or points. The real APR, or annual percentage rate, considers these costs as well as the interest rate of a loan. This repo help to reveal the true costs of loans through real APR.
